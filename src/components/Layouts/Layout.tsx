07b4c1a8ef577ccd150db896ee343c2de69fa227
Rizvi Mahmud <rizvimahmud1@gmail.com>
2022-03-28 20:53:55 +0600

    finalized design and layout

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -5,7 +5,7 @@ import { MotionDiv } from "./MotionDiv";
 export const Layout: React.FC = ({ children }) => {
   return (
     <div className="relative min-h-screen">
-      <div className="absolute inset-0 w-full blob z-[-1]" />
+      <div className="absolute inset-0 w-full blob z-[-1] blur-3xl" />
       <div className="relative z-0">
         <Nav />
         <main className="px-4 pt-16 mb-12">
